9196a083a34f28cfd7bd77e85dc7fec772319f97
jc.bernack@gmail.com <jc.bernack@gmail.com@f4bcd5f1-d22f-548d-702d-3a9aa84817de>
2011-12-08 19:24:45 +0000

    - added a bunch of icons

--- trunk/AnnoDesigner/MainWindow.xaml.cs
+++ trunk/AnnoDesigner/MainWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows;
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Media;
 using Microsoft.Windows.Controls;
 
@@ -11,20 +14,29 @@ namespace AnnoDesigner
         : Window
     {
         private AnnoObject _currentObject;
+        private List<string> _icons;
 
         public MainWindow()
         {
             InitializeComponent();
+            // add color presets
             colorPicker.StandardColors.Clear();
             colorPicker.StandardColors.Add(new ColorItem(Color.FromRgb(214, 49, 49), "red"));
             colorPicker.StandardColors.Add(new ColorItem(Color.FromRgb(171, 232, 107), "green"));
             colorPicker.StandardColors.Add(new ColorItem(Color.FromRgb(76, 106, 222), "blue"));
             colorPicker.StandardColors.Add(new ColorItem(Color.FromRgb(247, 150, 70), "orange"));
             colorPicker.StandardColors.Add(new ColorItem(Color.FromRgb(0, 0, 0), "black"));
+            // add icons
+            _icons = new List<string>(Directory.GetFiles(@"icons\", "*.png"));
         }
 
         private void WindowLoaded(object sender, RoutedEventArgs e)
         {
+            // add icons to the combobox
+            comboBoxIcon.Items.Clear();
+            comboBoxIcon.Items.Add(new ComboBoxItem { Content = "None" });
+            _icons.ForEach(_ => comboBoxIcon.Items.Add(new ComboBoxItem { Content = Path.GetFileNameWithoutExtension(_) }));
+            comboBoxIcon.SelectedIndex = 0;
         }
 
         private void MenuItemClick(object sender, RoutedEventArgs e)
@@ -42,7 +54,7 @@ namespace AnnoDesigner
                     Size = new Size(int.Parse(textBoxWidth.Text), int.Parse(textBoxHeight.Text)),
                     Color = colorPicker.SelectedColor,
                     Label = textBoxLabel.Text,
-                    Icon = comboBoxIcon.SelectedIndex > 0 ? @"images\Liquor.png" : null,
+                    Icon = comboBoxIcon.SelectedIndex == 0 ? null : _icons[comboBoxIcon.SelectedIndex],
                     Radius = int.Parse(textBoxRadius.Text)
                 };
                 // do some sanity checks
