6f91ea815078d686553fd610ecf55f1bf359fa2a
jc.bernack <jc.bernack@gmail.com>
2011-12-24 01:57:34 +0000

    - added default filename on the save export dialog (set to the name of the saved layout) - refactored some constants - added missing comments

--- AnnoDesigner/Constants.cs
+++ AnnoDesigner/Constants.cs
@@ -1,8 +1,35 @@
 ﻿namespace AnnoDesigner
 {
+    /// <summary>
+    /// Contains application wide constants
+    /// </summary>
     public static class Constants
     {
-        public static int Version = 6;
-        public static int FileVersion = 2;
+        /// <summary>
+        /// Version number of the application.
+        /// Will be increased with each release.
+        /// </summary>
+        public const int Version = 6;
+
+        /// <summary>
+        /// Version number of the saved file format.
+        /// Will be increased every time the file format is changed.
+        /// </summary>
+        public const int FileVersion = 2;
+
+        /// <summary>
+        /// The minimal grid size to which the user can zoom out.
+        /// </summary>
+        public const int GridStepMin = 8;
+
+        /// <summary>
+        /// The maximum grid size to which the user can zoom in.
+        /// </summary>
+        public const int GridStepMax = 100;
+
+        /// <summary>
+        /// The default grid size.
+        /// </summary>
+        public const int GridStepDefault = 20;
     }
 }
