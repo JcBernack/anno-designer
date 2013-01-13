01acb1e1c3ca886e9b4afbd5631c070be1abcfa3
jc.bernack <jc.bernack@gmail.com>
2013-01-13 03:17:48 +0000

    - refactored coordinate handling to geometry classes (point, size, rect) - added login and register form - minified styles.css

--- WebEdition/helpers.js
+++ WebEdition/helpers.js
@@ -1,21 +1,4 @@
-function RectContainsPoint(left, top, width, height, x, y) {
-    return left <= x && x < left + width && top <= y && y < top + height;
-}
-
-function length(x, y)
-{
-    return Math.sqrt(x*x + y*y);
-}
-
-Array.prototype.contains = function(obj) {
-    var i = this.length;
-    while (i--) {
-        if (this[i] === obj) {
-            return true;
-        }
-    }
-    return false;
-};
+// useful helper methods
 
 function trace(msg)
 {
@@ -24,55 +7,18 @@ function trace(msg)
         console.log(msg);
     }
 }
-// from http://stackoverflow.com/questions/4576724/dotted-stroke-in-canvas
-var CP = window.CanvasRenderingContext2D && CanvasRenderingContext2D.prototype;
-if (CP.lineTo) {
-    CP.dashedLine = function(x, y, x2, y2, da) {
-        if (!da) da = [10,5];
-        this.save();
-        var dx = (x2-x), dy = (y2-y);
-        var len = Math.sqrt(dx*dx + dy*dy);
-        var rot = Math.atan2(dy, dx);
-        this.translate(x, y);
-        this.moveTo(0, 0);
-        this.rotate(rot);
-        var dc = da.length;
-        var di = 0, draw = true;
-        x = 0;
-        while (len > x) {
-            x += da[di++ % dc];
-            if (x > len) x = len;
-            draw ? this.lineTo(x, 0): this.moveTo(x, 0);
-            draw = !draw;
-        }
-        this.restore();
+
+Array.prototype.last = function () {
+    return this[this.length - 1];
+};
+
+Array.prototype.remove = function (element)
+{
+    var i = this.indexOf(element);
+    if (i == -1)
+    {
+        return this;
     }
-}
-if (CP.arc) {
-    CP.arcMoveTo = function(x, y, radius, angle) {
-        this.moveTo(x+radius*Math.cos(angle), y+radius*Math.sin(angle));
-    }
-    CP.dashedArc = function(x, y, radius, angleStart, angleEnd, da) {
-        if (!da) da = [10,5];
-        var len = 2*Math.PI*radius;
-        var dc = da.length
-        var di = 0;
-        var draw = true;
-        var angle = angleStart;
-        while (angle < angleEnd)
-        {
-            var angleStep = angle + da[di++ % dc]/radius;
-            if (angleStep > angleEnd) angleStep = angleEnd;
-            if (draw)
-            {
-                this.arc(x, y, radius, angle, angleStep);
-            }
-            else
-            {
-                this.arcMoveTo(x, y, radius, angleStep);
-            }
-            angle = angleStep;
-            draw = !draw;
-        }
-    }
-}
+    this.splice(i, 1);
+    return this;
+};
